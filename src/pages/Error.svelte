ebcf1c4203630101bedec4bbc07a46df2c786ea3
Adnan <adnan_ghafoor94@hotmail.com>
2020-12-13 19:08:35 +0000

    Added a loading spinner

---
+++ src/pages/Error.svelte
@@ -0,0 +1,19 @@
+<style>
+  main {
+    text-align: center;
+  }
+  h1 {
+    font-weight: bold;
+    color: #333333;
+  }
+  p {
+    color: darkred;
+    padding: 10px 0;
+  }
+</style>
+
+<main>
+  <slot name="statusCode" />
+
+  <slot />
+</main>
--- src/pages/Portfolio.svelte
+++ src/pages/Portfolio.svelte
@@ -1,16 +1,12 @@
 <script>
-  import { onMount } from "svelte";
   import { fade, fly } from "svelte/transition";
   import axios from "axios";
   import Cards from "../components/Cards.svelte";
+  import Error from "./Error.svelte";
 
-  let items = [];
-  onMount(async () => {
-    const { data } = await axios.get(
-      "https://api.github.com/users/AGhafoor94/repos"
-    );
-    items = data;
-  });
+  const getGitHubRepos = async () =>
+    await axios.get("https://api.github.com/users/AGhafoor94/repo ");
+  let promise = getGitHubRepos();
 </script>
 
 <style>
@@ -21,18 +17,27 @@
   }
 </style>
 
-<main>
-  <div in:fly={{ y: 200, duration: 2000 }} out:fade|local class="cards">
-    {#each items as item, index}
-      <Cards
-        title={item.name}
-        subtitle={item.description}
-        link1={item.html_url}
-        link2={item.homepage}
-        id={index}>
-        <p slot="link-1-text">GitHub Url</p>
-        <p slot="link-2-text">Homepage</p>
-      </Cards>
-    {/each}
-  </div>
-</main>
+{#await promise}
+  <img src="/Portfolio/loading.svg" alt="Loading spinner" />
+{:then items}
+  <main>
+    <div in:fly={{ y: 200, duration: 2000 }} out:fade|local class="cards">
+      {#each items as item, index}
+        <Cards
+          title={item.name}
+          subtitle={item.description}
+          link1={item.html_url}
+          link2={item.homepage}
+          id={index}>
+          <p slot="link-1-text">GitHub Url</p>
+          <p slot="link-2-text">Homepage</p>
+        </Cards>
+      {/each}
+    </div>
+  </main>
+{:catch error}
+  <Error>
+    <h1 slot="statusCode">{error.response.status}</h1>
+    <p>{error.message}</p>
+  </Error>
+{/await}
